a341c6a9646ae26996bb971e9583850f25e99832
Justin Kumpe <jakumpe@kumpes.com>
2024-03-26 20:11:56 -0500

    -

--- app.py
+++ app.py
@@ -10,14 +10,20 @@ app = Flask(__name__)
 @app.route("/print_label/<int:customer_id>/<string:sku>/<string:label>/<int:qty>", methods=["POST"])
 def print_label(customer_id, sku, label, qty):
     """Print Label"""
-    printcommands.print_product_label(sku, label, qty, customer_id)
-    return "OK"
-
+    try: 
+        printcommands.print_product_label(sku, label, qty, customer_id)
+        return "OK"
+    except ValueError:
+        return f"Label Type {label} not supported"
 
 @app.route("/print_bundle/<int:customer_id>/<string:sku>/<int:qty>", methods=["POST"])
 def print_bundle(customer_id, sku, qty):
     """Print Label Bundle"""
-    printcommands.print_label_bundle(sku, qty, customer_id)
+    try:
+        printcommands.print_label_bundle(sku, qty, customer_id)
+        return "OK"
+    except:
+        return "Unknown Error"
 
 
 if __name__ == "__main__":
